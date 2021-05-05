9467c22bff6e9ee74ebda184ba37fb3eff334264
Jake Rankin <japankid.jake@gmail.com>
2021-05-05 15:18:02 -0500

    updated hero with bg image

--- src/homepage/Hero.jsx
+++ src/homepage/Hero.jsx
@@ -1,19 +1,27 @@
+import bgImage from '../assets/images/home-hero-bg.png';
+
 function Hero() {
-    return (
-      <section id="hero" className="m-8">
-        <div className="flex flex-col items-center">
-          <h1 className="text-2xl whitespace-nowrap pb-8">CRAFTSMEN DEDICATED TO DETAIL</h1>
-          <div id='greeting-card' className="p-2 bg-gray-300 bg-opacity-50 max-w-lg rounded-lg shadow-md">
-            <h2 className="text-lg">
-            Since 1922, Wisconsin Engraving Company is
-            constantly exploring new techniques and investing in 
-            research and development to give our customers the
-            competitive edge.
-            </h2>      
-          </div>
+  const styles = {
+    backgroundImage: `url(${bgImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  };
+
+  return (
+    <section id="hero" className="" style={styles}>
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl whitespace-nowrap p-8 pt-36">CRAFTSMEN DEDICATED TO DETAIL</h1>
+        <div id='greeting-card' className="p-2 px-3 mb-8 bg-gray-300 bg-opacity-30 max-w-lg rounded-lg shadow-md">
+          <h2 className="text-lg">
+          Since 1922, Wisconsin Engraving Company is
+          constantly exploring new techniques and investing in 
+          research and development to give our customers the
+          competitive edge.
+          </h2>      
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
